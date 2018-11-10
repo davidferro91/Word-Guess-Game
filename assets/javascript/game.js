a641e43358409c5a8f8fad69c68d1f07cd62644e
davidferro <david-ferro@sbcglobal.net>
2018-11-09 19:01:27 -0500

    variables declared

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+var winCount = 0;
+
+var lossCount = 0;
+
+var spaceWords = ["planet", "galaxy", "space", "interstellar", "astronaut", "mars", "venus", "mercury", "jupiter", "saturn", "uranus", "neptune", "pluto", "orbit", "star", "moon", "comet", "asteroid", "nebula", "satellite"];
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+document.getElementById("currentWord").addEventListener("keypress", function() {
+
+});
