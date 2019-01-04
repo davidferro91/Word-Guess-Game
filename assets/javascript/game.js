1ffac6153025ee3b387bbbcc0926c2819acf755d
davidferro <david-ferro@sbcglobal.net>
2019-01-04 15:21:37 -0500

    made fixes with javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,15 +5,16 @@ var guessNumber = 10;
 var guess = "";
 var alreadyGuessed = [ ];
 var correctCounter = 0;
+var beenReset = false;
 
-var spaceWords = ["planet", "galaxy", "space", "interstellar", "astronaut", "mars", "venus", "mercury", "jupiter", "saturn", "uranus", "neptune", "pluto", "orbit", "star", "moon", "comet", "asteroid", "nebula", "satellite", "antimatter", "light", "vacuum", "blazar", "gravity", "quasar", "solar", "starburst", "telescope", "ultraviolet", "extraterrestrial", "supernova", "sun", "cold", "giant", "radio", "pulsar", "protoplanetary", "neutron", "cluster", "microwaves", "radiation", "meteorite", "meteoroid", "meteor", "mass", "relativity", "electromagnetic", "lunar", "eclipse", "kuiper", "infrared", "galactic", "force", "elliptical", "earth", "matter", "energy", "cosmos", "corona", "dwarf", "atmosphere", "wormhole"];
+var spaceWords = ["planet", "galaxy", "space", "interstellar", "astronaut", "mars", "venus", "mercury", "jupiter", "saturn", "uranus", "neptune", "pluto", "orbit", "star", "moon", "comet", "asteroid", "nebula", "satellite", "antimatter", "light", "vacuum", "blazar", "gravity", "quasar", "solar", "starburst", "telescope", "ultraviolet", "supernova", "sun", "cold", "giant", "radio", "pulsar", "neutron", "cluster", "microwaves", "radiation", "meteorite", "meteoroid", "meteor", "mass", "relativity", "lunar", "eclipse", "kuiper", "infrared", "galactic", "force", "elliptical", "earth", "matter", "energy", "cosmos", "corona", "dwarf", "atmosphere", "wormhole"];
 
 var spaceWordNum = -1;
 var spaceWord = "";
 var nodeCounter = 1;
 
 
-document.body.addEventListener("keypress", function() {
+document.body.addEventListener("keyup", function(event) {
     if (spaceWordNum == -1) {
     spaceWordNum = Math.floor(Math.random() * spaceWords.length);
     console.log(spaceWordNum);
@@ -31,8 +32,8 @@ document.body.addEventListener("keypress", function() {
         
 
     } else {
-        var key = event.which||event.keyCode;
-        var letter = String.fromCharCode(key);
+        var keyPress = event.which || event.keyCode;
+        var letter = String.fromCharCode(keyPress).toLowerCase();
         console.log(letter);
         var stringCounter = 0;
 
@@ -40,7 +41,7 @@ document.body.addEventListener("keypress", function() {
         for (var i=0; i < spaceWord.length; i++) {
             if(spaceWord.charAt(i) == letter) {
                 var letterEl = document.createElement("li");
-                letterEl.setAttribute("class", "col-1 bigText");
+                letterEl.setAttribute("class", "col bigText");
                 var letterGuess = document.createTextNode("" + letter.toUpperCase());
                 letterEl.appendChild(letterGuess);
                 var item = document.getElementById("wordHolder").childNodes[i+nodeCounter];
@@ -59,11 +60,12 @@ document.body.addEventListener("keypress", function() {
     document.getElementById("instructions").innerHTML = "Type in a letter that you think is in the word!"
     document.getElementById("winCounter").innerHTML = "Wins: " + winCount;
     document.getElementById("lossCounter").innerHTML = "Losses: " + lossCount;
-    document.getElementById("guessRemaining").innerHTML = "Number of Guesses Remaining: " + guessNumber;
+    document.getElementById("guessRemaining").innerHTML = "Number of Incorrect Guesses Remaining: " + guessNumber;
     document.getElementById("lettersRemaining").innerHTML = "Letters Already Guessed: " + alreadyGuessed.toString().toUpperCase();
     if (correctCounter == spaceWord.length) {
-        document.getElementById("instructions").innerHTML = "You Win! The word was " + spaceWord.toUpperCase() + "! Press any key to play again!";
+        document.getElementById("instructions").innerHTML = "You Win! The word was " + spaceWord.toUpperCase() + "! Press any key or click New Word to play again!";
         winCount++;
+        document.getElementById("winCounter").innerHTML = "Wins: " + winCount;
         for (var i=0; i < spaceWord.length + nodeCounter; i++) {
             var reset = document.getElementById("wordHolder");
             reset.removeChild(reset.childNodes[0]);
@@ -73,10 +75,12 @@ document.body.addEventListener("keypress", function() {
         alreadyGuessed = [ ];
         nodeCounter = 0;
         correctCounter = 0;
+        beenReset = true;
     }
     if (guessNumber < 1) {
-        document.getElementById("instructions").innerHTML = "You Lose! Sorry! The word was " + spaceWord.toUpperCase() +". Press any key to play again!";
+        document.getElementById("instructions").innerHTML = "You Lose! Sorry! The word was " + spaceWord.toUpperCase() +". Press any key or click New Word to play again!";
         lossCount++;
+        document.getElementById("lossCounter").innerHTML = "Losses: " + lossCount;
         for (var i=0; i < spaceWord.length + nodeCounter; i++) {
             var reset = document.getElementById("wordHolder");
             reset.removeChild(reset.childNodes[0]);
@@ -86,24 +90,47 @@ document.body.addEventListener("keypress", function() {
         alreadyGuessed = [ ];
         nodeCounter = 0;
         correctCounter = 0;
+        beenReset = true;
     } 
     
 });
 
 document.getElementById("newGame").addEventListener("click", function() {
-    for (var i=0; i < spaceWord.length + nodeCounter; i++) {
-    var reset = document.getElementById("wordHolder");
-    reset.removeChild(reset.childNodes[0]);
+    if (beenReset == false || spaceWordNum != -1) {
+        for (var i=0; i < spaceWord.length + nodeCounter; i++) {
+            var reset = document.getElementById("wordHolder");
+            reset.removeChild(reset.childNodes[0]);
+        }
     }
+    beenReset = false;
     guessNumber = 10;
     spaceWordNum = -1;
     alreadyGuessed = [ ];
     nodeCounter = 0;
-    winCount = 0;
-    lossCount = 0;
-    document.getElementById("instructions").innerHTML = "Press any key to get started!"
+    document.getElementById("instructions").innerHTML = "Type in a letter that you think is in the word!"
     document.getElementById("winCounter").innerHTML = "Wins: " + winCount;
     document.getElementById("lossCounter").innerHTML = "Losses: " + lossCount;
-    document.getElementById("guessRemaining").innerHTML = "Number of Guesses Remaining: " + guessNumber;
+    document.getElementById("guessRemaining").innerHTML = "Number of Incorrect Guesses Remaining: " + guessNumber;
     document.getElementById("lettersRemaining").innerHTML = "Letters Already Guessed: " + alreadyGuessed.toString().toUpperCase();
+
+    spaceWordNum = Math.floor(Math.random() * spaceWords.length);
+    console.log(spaceWordNum);
+    
+    spaceWord = spaceWords[spaceWordNum];
+    console.log(spaceWord);
+    
+    for (var i=0; i<spaceWord.length; i++) {
+        guess = document.createElement("li");
+        guess.setAttribute("class", "col-1 bigText");
+        var textGuess = document.createTextNode("_");
+        guess.appendChild(textGuess);
+        document.getElementById("wordHolder").appendChild(guess);
+        }
+});
+
+document.getElementById("reset-button").addEventListener("click", function() {
+    winCount = 0;
+    lossCount = 0;
+    document.getElementById("winCounter").innerHTML = "Wins: " + winCount;
+    document.getElementById("lossCounter").innerHTML = "Losses: " + lossCount;
 });
